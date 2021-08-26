03a87a83287ac8c7dc15826d2c0d4904e4c77b6a
JJinuk <jitoor610@gmail.com>
2021-08-26 16:25:37 +0900

    0826

---
+++ 백준10250.py
@@ -0,0 +1,13 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for i in range(t):
+    h, w, n = map(int, sys.stdin.readline().split())
+    room = n//h + 1
+    floor = n % h
+
+    if n % h ==0:
+        room = n//h
+        floor = h
+    print(f'{floor*100+room}')
