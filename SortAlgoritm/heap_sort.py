e01931183aac8aa86cd556949513a3a7eaf929f3
JJinuk <jitoor610@gmail.com>
2021-10-06 22:05:02 +0900

    1006_heap_revise

--- SortAlgoritm/heap_sort.py
+++ SortAlgoritm/heap_sort.py
@@ -1,29 +1,38 @@
-def heapify(unsorted, index, heap_size):
-    largest = index
-    left_index = 2 * index + 1
-    right_index = 2 * index + 2
-    if left_index < heap_size and unsorted[left_index] > unsorted[largest]:
+def heapify(arr, n, i):
+    largest = i                 # 루트를 largest로 초기화
+    left_index = 2 * i + 1      # left = 2*i + 1
+    right_index = 2 * i + 2     # right = 2*i + 2
+
+    # 루트의 왼쪽 자식 확인
+    # 루트보다 클 경우
+    if left_index < n and arr[left_index] > arr[largest]:
         largest = left_index
 
-    if right_index < heap_size and unsorted[right_index] > unsorted[largest]:
+    # 루트의 오른쪽 자식 확인
+    # 루트보다 클 경우
+    if right_index < n and arr[right_index] > arr[largest]:
         largest = right_index
 
-    if largest != index:
-        unsorted[largest], unsorted[index] = unsorted[index], unsorted[largest]
-        heapify(unsorted, largest, heap_size)
+    # root를 바꾼다(필요하다면)
+    if largest != i:
+        arr[largest], arr[i] = arr[i], arr[largest]     # swap
+        heapify(arr, largest, n)
 
 
-def heap_sort(unsorted):
-    n = len(unsorted)
+def heap_sort(arr):
+    n = len(arr)
+    # 최대 힙을 만든다.
     for i in range(n // 2 - 1, -1, -1):
-        heapify(unsorted, i, n)
+        heapify(arr, i, n)
+
+    # 요소를 하나씩 뽑는다
     for i in range(n - 1, 0, -1):
-        unsorted[0], unsorted[i] = unsorted[i], unsorted[0]
-        heapify(unsorted, 0, i)
-    return unsorted
+        arr[0], arr[i] = arr[i], arr[0]         # swap
+        heapify(arr, 0, i)
+    return arr
 
 
 if __name__ == "__main__":
     user_input = input("Enter numbers separated by a comma:\n").strip()
-    unsorted = [int(item) for item in user_input.split(",")]
-    print(heap_sort(unsorted))
+    arr = [int(item) for item in user_input.split(",")]
+    print(heap_sort(arr))
