f0648adec6f69612876f167fa878791990c27270
JJinuk <jitoor610@gmail.com>
2022-02-08 22:25:00 +0900

    [0208] Algorithm - 입출력

--- BOJ/LV1/10172.py
+++
@@ -1,5 +0,0 @@
-print(r'|\_/|')
-print(r'|q p|   /}')
-print(r'( 0 )"""\ ')
-print(r'|"^"`    |')
-print(r'||_/=\\__|')
--- BOJ/LV1/10869.py
+++
@@ -1,9 +0,0 @@
-a, b = map(int, input().split())
-
-pls = a+b
-sub = a-b
-mul = a*b
-div = a//b
-res = a%b
-
-print(pls, sub, mul, div, res, sep='\n')
