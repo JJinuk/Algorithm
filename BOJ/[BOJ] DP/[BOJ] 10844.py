81d818453800f295ff01ab72997aa3acc923abb4
JJinuk <jitoor610@gmail.com>
2023-01-10 01:57:46 +0900

    [0110] BOJ - DP02

--- BOJ/[BOJ] DP/[BOJ] 10844.py
+++ BOJ/[BOJ] DP/[BOJ] 10844.py
@@ -1,16 +1,16 @@
 n = int(input())
 
-dp = [[0] * 10 for _ in range(n+1)]
-for i in range(1, 10):
+dp = [[0] * 10 for _ in range(n+1)]     # n개의 테이블
+for i in range(1, 10):                  # 초기화, n이 1인 경우(0번째)에 값을 처음에 입력해주겠다는 의미
     dp[1][i] = 1
 
-for j in range(2, n+1):
-    for k in range(10):
-        if k == 0:
-            dp[j][k] = dp[j-1][1]
-        elif k == 9:
-            dp[j][k] = dp[j-1][8]
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
         else:
-            dp[j][k] = dp[j-1][k-1] + dp[j-1][k+1]
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
 print(sum(dp[n]) % pow(10, 9))
